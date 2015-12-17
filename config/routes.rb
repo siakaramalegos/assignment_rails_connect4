7876522330b6cf0b82e6ed900a368e5042924219
Sia <siakaramalegos@gmail.com>
2015-12-17 11:50:01 -0600

    Adds home and about pages and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  get 'gamecenter' => 'pages#home'
+  get 'pages/home'
+  get 'pages/about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
